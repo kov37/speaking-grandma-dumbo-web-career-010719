f607818f174d3fdd4e2bcd0b00de02ca4ecb508b
kovner chalumo <kchalumo@gmail.com>
2018-11-25 00:03:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@ def speak_to_grandma(shout)
   if shout == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
   elsif shout == shout.upcase
-    'HUH, SPEAK UP, SONNY!'
+    'NO, NOT SINCE 1938'
   else 
-    'NO, NOT SINCE 1938!'
+    'HUH? SPEAK UP, SONNY!'
   end
 end 
